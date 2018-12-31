f521bba431bb20ca421eea99af74174e78512739
robaerd <robert.sowula9879@gmail.com>
2018-12-31 12:28:23 +0100

    added flycheck-irony mode, eldoc-mode. added smart ident/completion with TAB

---
+++ lisp/c-cpp-opjc-mode.el
@@ -0,0 +1,92 @@
+;; irony server
+(unless (package-installed-p 'irony)
+    (irony-install-server t))
+
+;; == irony-mode ==
+(use-package irony
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  :config
+  ;; replace the `completion-at-point' and `complete-symbol' bindings in
+  ;; irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  ; trigger completion at interesting places, such as after scope operator
+  ;     std::|
+  (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+  )
+
+;; == company-mode ==
+(use-package company
+  :ensure t
+  :defer t
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (use-package company-irony :ensure t :defer t)
+  (setq company-idle-delay              t
+	company-minimum-prefix-length   2
+	company-show-numbers            t
+	company-tooltip-limit           20
+	company-dabbrev-downcase        nil
+	company-backends                '((company-irony company-gtags))
+	)
+  :bind ("C-;" . company-complete-common)
+  )
+
+;; == flycheck-mode ==
+(use-package flycheck-irony
+	:ensure t
+	:defer t
+	:init 
+	(add-hook 'c++-mode-hook 'flycheck-mode)
+	(add-hook 'c-mode-hook 'flycheck-mode)
+	:config
+	(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+;(add-hook 'c++-mode-hook 'flycheck-mode)
+;(add-hook 'c-mode-hook 'flycheck-mode)
+;(eval-after-load 'flycheck
+;  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+
+;; == eldec-mode ==
+(add-hook 'irony-mode-hook 'irony-eldoc)
+
+
+;;;  bind TAB for indent-or-complete
+(defun irony--check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "->") t nil)))))
+(defun irony--indent-or-complete ()
+  "Indent or Complete"
+  (interactive)
+  (cond ((and (not (use-region-p))
+              (irony--check-expansion))
+         (message "complete")
+         (company-complete-common))
+        (t
+         (message "indent")
+         (call-interactively 'c-indent-line-or-region))))
+(defun irony-mode-keys ()
+  "Modify keymaps used by `irony-mode'."
+  (local-set-key (kbd "TAB") 'irony--indent-or-complete)
+  (local-set-key [tab] 'irony--indent-or-complete))
+(add-hook 'c-mode-common-hook 'irony-mode-keys)
+
+
+
+(provide 'c-cpp-opjc-mode)
