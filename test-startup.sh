eefb769c8c2bf28357b8a8a1d02de89003cdf173
robaerd <robert.sowula9879@gmail.com>
2019-01-03 00:16:54 +0100

    fix?

--- test-startup.sh
+++ test-startup.sh
@@ -8,6 +8,7 @@ echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
                               (url-show-status nil)
+                              (~/.emacs.d default-directory)
                               (user-init-file (expand-file-name "init.el"))
                               (load-path (delq default-directory load-path)))
                            (load-file user-init-file)
