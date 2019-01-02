1b77e761992f27a180447edda7cc2bc6ca0dc27f
robaerd <robert.sowula9879@gmail.com>
2019-01-02 23:38:28 +0100

    added keybindings for avy

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -1,7 +1,15 @@
 (use-package avy
   :ensure t
-  :bind (("M-s" . avy-goto-word-1))
-  :config
-  (avy-setup-default))
+  :bind ()
+  :config(progn
+	;; Makes it easier to see the candidates
+	(setq avy-background t)
+	;; Shows both the candidates over the text
+	(setq avy-styles-alist '((avy-goto-char-2 . at-full))))
+	:bind (;; Search by 2 chars
+	("C-'" . avy-goto-char-2)
+	;; Search by first char of a word
+	("C-\"" . avy-goto-word-1)
+	("M-s" . avy-goto-word-1)))
 
   (provide 'init-avy)
