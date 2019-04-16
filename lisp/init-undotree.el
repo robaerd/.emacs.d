57fef84265819451268dbb765598922ac814763e
robaerd <robert.sowula9879@gmail.com>
2019-04-16 11:49:35 +0200

    tweaked undo-tree: now showing diff and timestamp

--- lisp/init-undotree.el
+++ lisp/init-undotree.el
@@ -1,5 +1,16 @@
-(get-package 'undo-tree)
+(use-package undo-tree
+  :ensure t
+  :init
+  (setq undo-tree-visualizer-diff 1)
+  (setq undo-tree-visualizer-timestamps 1)
+  :config
+  (global-undo-tree-mode 1))
 
-(global-undo-tree-mode)
+
+;; make ctrl-z undo
+(global-set-key (kbd "C-z") 'undo)
+;; make ctrl-Z redo
+(defalias 'redo 'undo-tree-redo)
+(global-set-key (kbd "C-S-z") 'redo)
 
 (provide 'init-undotree)
