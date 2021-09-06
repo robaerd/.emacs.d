d95da1081b32df99556a5dddb0cf726e90abff66
Robert Sowula <robert@sowula.at>
2021-09-06 10:27:30 +0200

    Remove haskell mode

--- init.el
+++ init.el
@@ -80,7 +80,6 @@
 (require 'init-projectile) ; keybindings: S-(d | f | g | p)
 (require 'init-zygospore) ; focuse on current buffer (delete all other) + revert with C-x 1
 (require 'c-cpp-opjc-mode)
-(require 'init-haskell-mode)
 (require 'init-dockerfile-mode)
 (require 'init-magit)
 (require 'init-smartparens)
